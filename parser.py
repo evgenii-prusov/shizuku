d73bed216b8a6192b7262b193fe70e683f753c1d
Evgenii Prusov <prusove@gmail.com>
2026-08-22 21:48:11 +0200

    Fix all test failures for the parser

--- parser.py
+++ parser.py
@@ -1,6 +1,10 @@
 # b'GET / HTTP/1.1\r\nHost: example.com\r\n\r\n'
 # b'POST /submit HTTP/1.1\r\nHost: h\r\nContent-Length: 5\r\n\r\nhello'
 from request import Request
+import re
+
+
+_VERSION = re.compile(r"^HTTP/\d\.\d$")
 
 
 class MalformedRequest(Exception): ...
@@ -18,13 +22,20 @@ class ParserState:
         except ValueError:
             return None
         if terminator_ind == 0:
-            raise MalformedRequest()
+            raise MalformedRequest('bare-lf-is-rejected')
         line = parser.buffer[: terminator_ind - 1]
+        del parser.buffer[: terminator_ind + 1]
         if line == b"":
             return b""
-        del parser.buffer[: terminator_ind + 1]
         return line
 
+    @staticmethod
+    def _take_bytes(parser, n: int) -> bytes | None:
+        if len(parser.buffer) < n:
+            return None
+        body = parser._body = parser.buffer[:n]
+        del parser.buffer[:n]
+        return body
 
 class StartLineState(ParserState):
     @staticmethod
@@ -33,15 +44,23 @@ class StartLineState(ParserState):
         line = ParserState._take_line(parser)
         if line is None:
             return None
-        if len(line.split(b" ")) != 3:
-            raise MalformedRequest()
-
-        parser._method, parser._target, parser._version = [
-            str(s, encoding="ascii") for s in line.split(b" ")
-        ]
+        
+        line_fields = line.split(b' ')
+        if len(line_fields) == 2:
+            raise MalformedRequest('request-line-two-fields')
+        elif len(line_fields) == 4:
+            raise MalformedRequest('request-line-four-fields')
+        elif len(line.split(b" ")) == 3:
+            parser._method, parser._target, parser._version = [
+                str(s, encoding="ascii") for s in line_fields
+            ]
+        if parser._method == '':
+            raise MalformedRequest('empty-method')
+        if not _VERSION.match(parser._version):
+            raise MalformedRequest('unparseable-version')
+        
         parser.new_state(HeadersState)
-        parser.feed(b"")
-        return None
+        return parser.feed(b"")
 
 
 class HeadersState(ParserState):
@@ -53,20 +72,37 @@ class HeadersState(ParserState):
             return None
         while line and len(line) > 0:
             try:
-                k, v = line.split(b":")
+                colon_ind: int = line.index(b':')
+                k = line[:colon_ind]
+                v = line[colon_ind + 1:]
+                #k, v = line.split(b":")
                 k = str(k, encoding="ascii").lower()
                 v = str(v, encoding="ascii").strip()
+                print(f'k: {k} v: {v}')
+                if k in parser._headers:
+                    curr_val = parser._headers[k]
+                    print(f'curr_val: {curr_val}')
+                    v = curr_val + ',' + v
+                    print(parser._headers) 
                 parser._headers[k] = v
+                if 'content-length' in parser._headers:
+                    content_length: int = 0
+                    try:
+                        content_length = int(parser._headers['content-length'])
+                    except ValueError:
+                        raise MalformedRequest('Content-Length should be number')
+                    if content_length < 0:
+                        raise MalformedRequest('Content-Length should be > 0')
                 line = ParserState._take_line(parser)
 
             except ValueError:
-                raise MalformedRequest
+                raise MalformedRequest('Incorrect header format')
 
         if line is None:
             return None
         if line == b"":
             parser.new_state(BodyState)
-            parser.feed(b"")
+            return parser.feed(b"")
         return None
 
 
@@ -74,18 +110,15 @@ class BodyState(ParserState):
     @staticmethod
     def feed(parser, data: bytes) -> None:
         parser.buffer += data
-        line = ParserState._take_line(parser)
-        if line is None:
+        body = ParserState._take_bytes(
+            parser,
+            int(parser._headers.get('content-length', 0))
+        )
+        if body is None:
             return None
-        content_length = self._headers.get('content_lenght', 0)
-        if len(line) != content_length:
-            raise MalformedRequest()
-        else:
-            self._body = line
-            parser.new_state(FinalState)
-            parser.feed(b"")
-        return None
-            
+        parser._body = body
+        parser.new_state(FinalState)
+        return parser.feed(b"")
 
 
 class FinalState(ParserState):
@@ -95,10 +128,14 @@ class FinalState(ParserState):
             method = parser._method,
             target = parser._target,
             headers = parser._headers,
-            body = parser._body
+            body = bytes(parser._body)
         )
         return request
 
+class ErrorState(ParserState):
+    @staticmethod
+    def feed(parser, data: bytes) -> None | Request:
+        return None
 
 class RequestParser:
     def __init__(self):
@@ -118,36 +155,30 @@ class RequestParser:
         return self._state._take_line(self)
 
     def feed(self, data: bytes) -> Request | None:
-        return self._state.feed(self, data)
+        request: Request | None = None
+        try:
+            request = self._state.feed(self, data)
+        except MalformedRequest as e:
+            self.buffer = bytearray()
+            if self.error is None:
+                self.error = str(e)
+            self.new_state(ErrorState)
+        return request
 
     def __repr__(self):
         return (
             f"STATE: {str(self._state.__name__)}\n"
             f"  buffer: {self.buffer}\n"
-            f"  method: {self._method} target: {self._target} version: {self._version}\n"
+            f"    method: {self._method}\n" 
+            f"    target: {self._target}\n"
+            f"    version: {self._version}\n"
             f"  headers: {self._headers}\n"
+            f"  body: {str(self._body, encoding='ascii')}"
         )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = RequestParser()
-    chunk1 = b"GET / "
-    chunk2 = b"HT"
-    chunk3 = b"TP/1.1\r\n"
-    chunk4 = b"Host: "
-    chunk5 = b"dtask.dev\r"
-    chunk6 = b"\n"
-    parser.feed(chunk1)
-    parser.feed(chunk2)
-    parser.feed(chunk3)
-    parser.feed(chunk4)
-    parser.feed(chunk5)
-    parser.feed(chunk6)
-    chunk7 = b"Content-Length: 5\r"
-    parser.feed(chunk7)
-    chunk8 = b"\n"
-    parser.feed(chunk8)
-    print(parser)
-    chunk9 = b"12345"
-    
-    
+    chunk = b'GET /\r\n\r\n'
+    request = parser.feed(chunk)
+
